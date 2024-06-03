59466f012cd1cb55f9a5a18029b5f8dbba194c79
Dawid Haładus <alerinpoland@gmail.com>
2024-06-04 01:37:36 +0200

    First commit

---
+++ Source/Zonit.Extensions.Databases.Examples/Entities/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace Zonit.Extensions.Databases.Examples.Entities;
+
+public class Blog
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime Created { get; private set; } = DateTime.UtcNow;
+}
